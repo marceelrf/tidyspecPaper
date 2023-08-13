1805d044de65a45b1ef95991d2f91d1f87e54ba0
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2023-08-13 13:03:25 -0300

    update 20230813

---
+++ extra.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+UV <- modeldatatoo::data_chimiometrie_2019()
+
+
+UV %>%
+  pivot_longer(cols = starts_with("wvlgth"),
+               names_to = "code",
+               values_to = "Wavelength") %>%
+  pivot_longer(cols = soy_oil:barley,
+               names_to = "type",values_to = "val") %>%
+  ggplot(aes(x = Wavelength, y = val, col = type)) +
+  geom_line()
